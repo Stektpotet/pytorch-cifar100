c9c5ab078519358e39b410c24200daae999a0ddb
Halvor <stektpotet@gmail.com>
2021-11-01 13:47:39 +0100

    Combine Standard and QMargin training, controlled by flag -qmargin

--- train.py
+++ train.py
@@ -1,10 +1,3 @@
-# train.py
-#!/usr/bin/env	python3
-
-""" train network using pytorch
-
-author baiyu
-"""
 
 import argparse
 import os
@@ -16,9 +9,66 @@ import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
 
 from conf import settings
+from qmargin_sampling import qmargin_accumulate
 from utils import get_network, get_training_dataloader, get_test_dataloader, WarmUpLR, \
     most_recent_folder, most_recent_weights, last_epoch, best_acc_weights
 
+
+def train_qmargin(epoch):
+
+    start = time.time()
+    net.train()
+    batch_index = 0
+    for batch_index, (images, labels) in enumerate(qmargin_accumulate(cifar100_training_loader, net, args.b, args.gpu, args.quantile, args.margin)):
+        if args.gpu:
+            labels = labels.cuda()
+            images = images.cuda()
+
+        optimizer.zero_grad()
+        outputs = net(images)
+        loss = loss_function(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        n_iter = (epoch - 1) * len(cifar100_training_loader) + batch_index + 1
+
+        last_layer = list(net.children())[-1]
+        for name, para in last_layer.named_parameters():
+            if 'weight' in name:
+                writer.add_scalar('LastLayerGradients/grad_norm2_weights', para.grad.norm(), n_iter)
+            if 'bias' in name:
+                writer.add_scalar('LastLayerGradients/grad_norm2_bias', para.grad.norm(), n_iter)
+
+        print('QMargin Training Epoch: {epoch} [{trained_samples}/{total_samples}]\tLoss: {:0.4f}\tLR: {:0.6f}'.format(
+            loss.item(),
+            optimizer.param_groups[0]['lr'],
+            epoch=epoch,
+            trained_samples=(batch_index + 1) * args.b,
+            total_samples=len(cifar100_training_loader.dataset)
+        ))
+
+        #update training loss for each iteration
+        writer.add_scalar('Train/loss', loss.item(), n_iter)
+
+        # if epoch <= args.warm:
+            # warmup_scheduler.step()
+
+    print('QMargin Training Epoch: {epoch}\t{percentage:.2f}% of samples used...'.format(
+            epoch=epoch,
+            percentage=((batch_index+1) * args.b)*100/len(cifar100_training_loader.dataset)) +
+          '\nDropped {dropped_samples} out of {total_samples} samples.'.format(
+            dropped_samples=len(cifar100_training_loader.dataset) - ((batch_index+1) * args.b),
+            total_samples=len(cifar100_training_loader.dataset)
+    ))
+
+    for name, param in net.named_parameters():
+        layer, attr = os.path.splitext(name)
+        attr = attr[1:]
+        writer.add_histogram("{}/{}".format(layer, attr), param, epoch)
+
+    finish = time.time()
+
+    print('epoch {} training time consumed: {:.2f}s'.format(epoch, finish - start))
 
 def train(epoch):
 
@@ -56,8 +106,8 @@ def train(epoch):
         #update training loss for each iteration
         writer.add_scalar('Train/loss', loss.item(), n_iter)
 
-        if epoch <= args.warm:
-            warmup_scheduler.step()
+        # if epoch <= args.warm:
+        #     warmup_scheduler.step()
 
     for name, param in net.named_parameters():
         layer, attr = os.path.splitext(name)
@@ -111,17 +161,21 @@ def eval_training(loader, epoch=0, tb=True, tag='Test'):
 
     return correct.float() / len(loader.dataset)
 
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-net', type=str, required=True, help='net type')
     parser.add_argument('-gpu', action='store_true', default=False, help='use gpu or not')
+    parser.add_argument('-qmargin', action='store_true', default=False, help='use qmargin selection')
     parser.add_argument('-b', type=int, default=128, help='batch size for dataloader')
-    parser.add_argument('-warm', type=int, default=0, help='warm up training phase')
+    parser.add_argument('-warm', type=int, default=-1, help='warm up training phase')
     parser.add_argument('-lr', type=float, default=0.05, help='initial learning rate')
+    parser.add_argument('-q', '--quantile', type=float, default=1.0, help='quantile to evaluate margin selection with')
+    parser.add_argument('-m', '--margin', type=float, default=0.4, help='selection margin')
     parser.add_argument('-resume', action='store_true', default=False, help='resume training')
     args = parser.parse_args()
-
+    print(args)
     net = get_network(args)
 
     #data preprocessing:
@@ -152,9 +206,11 @@ if __name__ == '__main__':
 
     loss_function = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=args.lr)
+    # print(optimizer.param_groups)
+    # exit()
     # train_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=settings.MILESTONES, gamma=0.2) #learning rate decay
     iter_per_epoch = len(cifar100_training_loader)
-    warmup_scheduler = WarmUpLR(optimizer, iter_per_epoch * args.warm)
+    # warmup_scheduler = WarmUpLR(optimizer, iter_per_epoch * args.warm)
 
     if args.resume:
         recent_folder = most_recent_folder(os.path.join(settings.CHECKPOINT_PATH, args.net), fmt=settings.DATE_FORMAT)
@@ -173,7 +229,7 @@ if __name__ == '__main__':
     #since tensorboard can't overwrite old values
     #so the only way is to create a new tensorboard log
     writer = SummaryWriter(log_dir=os.path.join(
-            settings.LOG_DIR, args.net, settings.TIME_NOW))
+            settings.LOG_DIR, args.net, 'qmargin' if args.qmargin else 'standard', settings.TIME_NOW))
     input_tensor = torch.Tensor(1, 3, 32, 32)
     if args.gpu:
         input_tensor = input_tensor.cuda()
@@ -204,16 +260,18 @@ if __name__ == '__main__':
 
         resume_epoch = last_epoch(os.path.join(settings.CHECKPOINT_PATH, args.net, recent_folder))
 
+    train_func = train_qmargin if args.qmargin else train
+
     for epoch in range(1, settings.EPOCH + 1):
         if epoch > args.warm:
-            # train_scheduler.step(epoch)
+            # train_scheduler.step()
             pass
 
         if args.resume:
             if epoch <= resume_epoch:
                 continue
 
-        train(epoch)
+        train_func(epoch)
         acc = eval_training(cifar100_test_loader, epoch)
         if epoch % 5 == 0:
             eval_training(cifar100_training_unaugmented_loader, epoch, tag='Train')
